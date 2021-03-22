3abe88c92d15764b128b066071ae6460b5752075
ospination <ospina.andres@gmaIl.com>
2021-03-22 16:59:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 
+$(document).ready(function(){
 var partidos = ["Perros","Gatos","Caballos","Lobos","Aguilas","Zarigueyas"];
 var votos = [];
 var i;
 for (i = 0; i < partidos.length; i++) {
   var x = Math.floor((Math.random() * 100000) + 100);
   votos.push(x);
-} 
-document.getElementById("partidos").innerHTML = partidos;
-document.getElementById("votos").innerHTML = votos;
-table = document.querySelector('table tbody');
-var tabla = [partidos,votos.sort(function (a, b) {  return b - a;  })];
-var r = partidos.map(function(col, i) {
-  return tabla.map(function(row) {
-    return row[i];
-  });
-});
-r.forEach(function(e) {
-  table.innerHTML += '<tr><td>' + e[0] + '</td><td>' + e[1] + '</td></tr>'
-})
-document.getElementById("tabla").innerHTML = tabla;
+}
+var partidos = partidos.sort( () => .5 - Math.random() );
+var votos = votos.sort(function (a, b) {  return b - a;  });
+var bodyString = '';
+    $.each(partidos, function(index, partidos) {
+        bodyString += ('<tr><td>'+partidos+'</td><td>'+votos[index]+'</td></tr>');
+    });
+    $('.partidos tbody').html(bodyString);
+    });
+
